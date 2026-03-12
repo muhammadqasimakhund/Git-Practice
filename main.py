e0dff067e9742e7f0fef5002853bd2aae5dc614f
Muhammad Qasim Akhund <akhundqasim1011@gmail.com>
2026-03-12 23:10:35 +0500

    Add Fibonacci function and fix print statement for add function

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ def add(a, b):
     return a + b
 print(add(1, 2))
 print(add(3, 4))
+
+# Write a function to create a febonacci sequencedef fibonacci(n):
+def fibonacci(n):
+    sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
+print(fibonacci(10))
