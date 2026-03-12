066c60eb40fe2a4eb8e1b722f319981a648c56fe
Muhammad Qasim Akhund <111875455+muhammadqasimakhund@users.noreply.github.com>
2026-03-12 23:01:49 +0500

    InitialCommit

---
+++ index.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+print("Hello, World!")
+
+# This is a simple Python script that prints "Hello, World!" twice.
+def greet():
+    print("Hello, World!")
+    
+greet()
+greet()
---
+++ main.py
@@ -0,0 +1,8 @@
+print(1+2)
+print(3+4)
+
+# Write function to add two numbers
+def add(a, b):
+    return a + b
+print(add(1, 2))
+print(add(3, 4))
